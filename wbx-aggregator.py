d320f7761f2fc9ab9292dbc26f03cc7d3ad90fc1
Hugo <Hugo>
2020-04-21 13:32:43 -0700

    update

--- wbx-aggregator.py
+++ wbx-aggregator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import re, os, glob, sys
+import re, os, glob, sys, jinja2, yaml
+from jinja2 import *
+from jinja2.environment import *
 
 
 vpls = re.compile(r'(vpls\s\d.+\n.*\n.*\n.*\n.*\n.*\n.*)')
@@ -88,6 +90,7 @@ def globfindfile(regex):
     except Exception as e:
         print(f"Something went wrong, {e}")
 
+
 def dictconvert(lst):
     ''' Convert a list to dict
     '''
@@ -95,12 +98,56 @@ def dictconvert(lst):
         res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
         return res_dct
     except Exception as e:
-        print(f"found an issue with dictconvert function. {e}")
+        print(f"found an issue with dictconvert function, {e}")
+
+def fldrsetup():
+    try:
+        # Create the generated sow folder + node folder.
+        # Create Temp Folder & Temp File
+        hostname = exhostid()  # extract hostname          
+        createFolder('Generated-SOW/'+hostname)
+        createFolder('Temp')
+    except Exception as e:
+        print(f"Error with fldrsetup, {e}")
+
+def confgen():
+    ''' Initial load of dir, config gen w/ jinja2/yaml
+    '''
+    try:
+        # Initial setup of the folder structure.
+        fldrsetup()
+        # ----------------------
+        # Yaml/inja Files:
+        # ----------------------
+        yaml_file = 'vars.yml'
+        jinja_template = 'templates/main.j2'
+        # ----------------------
+        # Parse the YAML file
+        with open(yaml_file) as f:
+            vars = yaml.load(f, Loader=yaml.FullLoader)  # Converts YAML file to dictionary
+
+        ENV = Environment(loader=FileSystemLoader([".", "./templates/"]),trim_blocks=True, lstrip_blocks=True)
+
+        with open(jinja_template, encoding='UTF-8') as f:
+            template_file = f.read()
+            template = ENV.from_string(template_file)
+        
+        config = (template.render(vars))
+        # print(config)
+
+        hostname = exhostid()  # extract hostname          
+        BASE = os.getcwd()
+        TEMPFILE = BASE+'/Generated-SOW/'+hostname+'/'+hostname+'.txt'
+        confgen =open(TEMPFILE, 'w+')
+        confgen.write(config)
+
+    except Exception as e:
+        print(f"found an issue with the confgen function, {e}")
+
 
 def vplsextract(compiledregex, file):
     '''
-    Utilizes re.findall to extract values and strip space and split with the 'no shutdown' keyword
-    at the end of the regex match for each vpls instance, including saps and descriptions.
+    Utilizes re.findall to extract values and strip the regex match for each vpls instance, including saps and descriptions.
     '''
     try:
         valuextract = re.findall(compiledregex, file)
@@ -109,123 +156,98 @@ def vplsextract(compiledregex, file):
     except Exception as e:
         print(f"found an issue with vplsextract function. {e}")
 
-def vplslistmodify():
+def vplslist():
     try:
         # Passing the globfindfile function, to find the file and read it.
         with open((globfindfile("*.log")), 'r+', encoding='utf-8') as f_rd:
             contents = f_rd.read()
 
-            hostname = exhostid()
-            # Change into temp folder and create an empty file to offload the vpls info
-            # and manipulate the file with regex.
-            BASE = os.getcwd()
-            createFolder('Temp')
-            TEMPFILE = BASE+'/Temp/vplsinfo-temp.txt'
-            tempfl=open(TEMPFILE, 'w+')
+            TEMPFILE = os.getcwd()+'/Temp/temp.txt'
+            tempfl = open(TEMPFILE, 'w+')
 
             #Run the vplsextract and loop through it. Write the contents to a temp file.
             vplsinfo = vplsextract(vpls,contents)
-
 
             for line in vplsinfo:
                 tempfl.writelines(line+'\n')
             # Close the temp file.
             tempfl.close()
+
+    except Exception as e:
+        print (f"Error with vplslist or initial temp file creation, {e}")    
+
+def vplspassover():
+    # Pass over the  temp file and remove items which we need to eliminate to create a
+    # working dictionary. Once the list is able to be split and converted into a dictionary, loop through it and extract the values to create the script
+    # to remove all the saps and shut down and remove the vpls.
+
+    try:
+        # Pull the hostname and identify the temp file.
+        hostname = exhostid()
+        TEMPFILE = os.getcwd()+'/Temp/temp.txt'
+
+        with open(TEMPFILE, 'r+', encoding='utf-8') as f_rd:
+            passover = f_rd.read()
+
+            xt = re.compile(r'(exit)')
+            crt = re.compile(r'(create)')
+            lb = re.compile(r'(\n)')
+            nsht = re.compile(r'(no shutdown)')
+            sp = re.compile(r'(\s+)')
+            lagsap = re.compile(r'(sap\slag)') # Replace the second sap in the VPLS, to avoid overwritting key when converting to dictionary.
+            # -----------------
+            PASS1 = re.sub(xt,'', passover)
+            PASS2 = re.sub(crt,'',PASS1)
+            PASS3 = re.sub(lb,'',PASS2)
+            PASS4 = re.sub(sp,' ', PASS3)
+            PASS5 = re.sub(lagsap,'lag-out lag', PASS4)
+
+            PASS6 = list(PASS5.split('no shutdown')) # Splits all the extracted VPLS's by the 'no shutdown' found inside the text str
             
-            # Pass over the  temp file and remove items which we need to eliminate to create a
-            # working dictionary. Once the list is able to be split and converted into a dictionary, loop through it and extract the values to create the script
-            # to remove all the saps and shut down and remove the vpls.
+            #Splitting leaves 1 empty list at the end, simply pop it out.
+            PASS6.pop()
 
-            with open(TEMPFILE, 'r+', encoding='utf-8') as f_rd:
-                passover = f_rd.read()
+            BASE = os.getcwd()
+            TEMPFILE = BASE+'/Generated-SOW/'+hostname+'/'+hostname+'.txt'
+            tempfl=open(TEMPFILE, 'a')
 
-                xt = re.compile(r'(exit)')
-                crt = re.compile(r'(create)')
-                lb = re.compile(r'(\n)')
-                nsht = re.compile(r'(no shutdown)')
-                sp = re.compile(r'(\s+)')
-                lagsap = re.compile(r'(sap\slag)') # Replace the second sap in the VPLS, to avoid overwritting key when converting to dictionary.
-                # -----------------
-                PASS1 = re.sub(xt,'', passover)
-                PASS2 = re.sub(crt,'',PASS1)
-                PASS3 = re.sub(lb,'',PASS2)
-                PASS4 = re.sub(sp,' ', PASS3)
-                PASS5 = re.sub(lagsap,'lag-out lag', PASS4)
+            try:
+                for x in PASS6:
+                    vplsdict = (dictconvert(x.split()))
+                    # print(vplsdict)
+                    if '1' in vplsdict['customer']:
+                        tempfl.write(f"echo Migrating service-id {vplsdict['vpls']}\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} shutdown\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} no description\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['sap']} shutdown\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['sap']}\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['lag-out']} shutdown\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['lag-out']}\n")
+                        tempfl.write(f"/configure service no vpls {vplsdict['vpls']}\n")
+                        tempfl.write(f"/configure service vpls 300\n")
+                        tempfl.write(f"/configure service vpls 300 sap {vplsdict['sap']} create\n")
+                        tempfl.write(f"/configure service vpls 300 sap {vplsdict['sap']} no shutdown\n")
+                    elif '4' in vplsdict['customer']:
+                        tempfl.write(f"echo Migrating service-id {vplsdict['vpls']}\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} shutdown\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} no description\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['sap']} shutdown\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['sap']}\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['lag-out']} shutdown\n")
+                        tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['lag-out']}\n")
+                        tempfl.write(f"/configure service no vpls {vplsdict['vpls']}\n")
+                        tempfl.write(f"/configure service vpls 400\n")
+                        tempfl.write(f"/configure service vpls 400 sap {vplsdict['sap']} create\n")
+                        tempfl.write(f"/configure service vpls 400 sap {vplsdict['sap']} no shutdown\n")
 
-                PASS6 = list(PASS5.split('no shutdown')) # Splits all the extracted VPLS's by the 'no shutdown' found inside the text str
-                
-                #Splitting leaves 1 empty list at the end, simply pop it out.
-                PASS6.pop()
+            except Exception as e:
+                print(e)
 
-                BASE = os.getcwd()
-                TEMPFILE = BASE+'/Temp/''SOW-'+hostname+'.txt'
-                tempfl=open(TEMPFILE, 'w+')
-
-                try:
-                    for x in PASS6:
-                        vplsdict = (dictconvert(x.split()))
-                        # print(vplsdict)
-                        if '1' in vplsdict['customer']:
-                            # print(f"echo Migrating service-id {vplsdict['vpls']}")
-                            # print(f"/configure service vpls {vplsdict['vpls']} shutdown")
-                            # print(f"/configure service vpls {vplsdict['vpls']} no description")
-                            # print(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['sap']} shutdown")
-                            # print(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['sap']}")
-                            # print(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['lag-out']} shutdown")
-                            # print(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['lag-out']}")
-                            # print(f"/configure service no vpls {vplsdict['vpls']}")
-                            # print(f"/configure service vpls 300")
-                            # print(f"/configure service vpls 300 sap {vplsdict['sap']} create")
-                            # print(f"/configure service vpls 300 sap {vplsdict['sap']} no shutdown")
-                            # print(f"/configure service vpls 300 sap lag-31:300 create") # Pass variable  of chosen LAG with openpyxl / excel(cq)
-                            # print(f"/configure service vpls 300 sap lag-31:300 no shutdown\n")
-                            tempfl.write(f"echo Migrating service-id {vplsdict['vpls']}\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} shutdown\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} no description\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['sap']} shutdown\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['sap']}\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['lag-out']} shutdown\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['lag-out']}\n")
-                            tempfl.write(f"/configure service no vpls {vplsdict['vpls']}\n")
-                            tempfl.write(f"/configure service vpls 300\n")
-                            tempfl.write(f"/configure service vpls 300 sap {vplsdict['sap']} create\n")
-                            tempfl.write(f"/configure service vpls 300 sap {vplsdict['sap']} no shutdown\n")
-                            tempfl.write(f"/configure service vpls 300 sap lag-31:300 create\n") # Pass variable  of chosen LAG with openpyxl / excel(cq)
-                            tempfl.write(f"/configure service vpls 300 sap lag-31:300 no shutdown\n")
-                        elif '4' in vplsdict['customer']:
-                            # print(f"echo Migrating service-id {vplsdict['vpls']}")
-                            # print(f"/configure service vpls {vplsdict['vpls']} shutdown")
-                            # print(f"/configure service vpls {vplsdict['vpls']} no description")
-                            # print(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['sap']} shutdown")
-                            # print(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['sap']}")
-                            # print(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['lag-out']} shutdown")
-                            # print(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['lag-out']}")
-                            # print(f"/configure service no vpls {vplsdict['vpls']}")
-                            # print(f"/configure service vpls 400")
-                            # print(f"/configure service vpls 400 sap {vplsdict['sap']} create")
-                            # print(f"/configure service vpls 400 sap {vplsdict['sap']} no shutdown")
-                            # print(f"/configure service vpls 400 sap lag-31:400 create") #  Pass variable  of chosen LAG with openpyxl / excel(cq)
-                            # print(f"/configure service vpls 400 sap lag-31:400 no shutdown\n")
-                            tempfl.write(f"echo Migrating service-id {vplsdict['vpls']}\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} shutdown\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} no description\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['sap']} shutdown\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['sap']}\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} sap {vplsdict['lag-out']} shutdown\n")
-                            tempfl.write(f"/configure service vpls {vplsdict['vpls']} no sap {vplsdict['lag-out']}\n")
-                            tempfl.write(f"/configure service no vpls {vplsdict['vpls']}\n")
-                            tempfl.write(f"/configure service vpls 400\n")
-                            tempfl.write(f"/configure service vpls 400 sap {vplsdict['sap']} create\n")
-                            tempfl.write(f"/configure service vpls 400 sap {vplsdict['sap']} no shutdown\n")
-                            tempfl.write(f"/configure service vpls 400 sap lag-31:400 create\n") #  Pass variable  of chosen LAG with openpyxl / excel(cq)
-                            tempfl.write(f"/configure service vpls 400 sap lag-31:400 no shutdown\n")
-
-                except Exception as e:
-                    print(e)
-
-                tempfl.close()
+            tempfl.close()
 
     except Exception as e:
         print(f"Something went wrong with vplslist modify, {e}")
 
-vplslistmodify()
+confgen()
+vplslist()
+vplspassover()
